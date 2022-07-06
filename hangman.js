793db568de5fcf6438f3e59e9f1b0f58e78d3f36
unknown <sazwerling@gmail.com>
2022-07-06 11:57:44 -0500

    make guess method

--- hangman.js
+++ hangman.js
@@ -1,26 +1,44 @@
-const Hangman = function(word, numGuesses, guessed = []){
-    this.word = word.toLowerCase().split('');
-    this.numGuesses = numGuesses;
-    this.guessed = guessed;
-} 
+// CREATE A METHOD FOR MAKING A GUESS.
+// Should accept a character for guessing.
+// Should add unique guesses to list of guesses.
+// Should decrement the guesses left if a unique guess is not a match.
 
-Hangman.prototype.getPuzzle = function(){
-    let puzzle = '';
-    this.word.forEach((letter) => {
-        if(this.guessed.includes(letter) || letter === ' '){
-            puzzle += letter
-        } else {
-            puzzle += '*'
-        }
-    })
-    return puzzle;
-}
+const Hangman = function (word, numGuesses) {
+   this.word = word.toLowerCase().split("");
+   this.numGuesses = numGuesses;
+   this.guessed = [];
+};
 
-const game1 = new Hangman("Cat", 3, ['c', 't'])
-const game2 = new Hangman("foot apparel", 200, ['f', 'o', 'p'])
+Hangman.prototype.makeGuess = function (letter) {
+   letter = letter.toLowerCase();
+   if (!this.guessed.includes(letter)) { // if unique guess, add to guessed array
+      this.guessed.push(letter);
+      if (!this.word.includes(letter)) { // nested if statement
+         this.numGuesses--;              // if unique guess AND if incorrect guess
+      }
+   }
+};
 
-// 1. Set word instance property as an array of lower case letters.
-// 2. Set up another instance property to store guessed letters.
-// 3. Create a method that gives you the word puzzle back. // getPuzzle()
+Hangman.prototype.getPuzzle = function () {
+   let puzzle = "";
+   this.word.forEach((letter) => {
+      if (this.guessed.includes(letter) || letter === " ") {
+         puzzle += letter;
+      } else {
+         puzzle += "*";
+      }
+   });
+   return puzzle;
+};
 
-console.log(game2.getPuzzle())
+const game1 = new Hangman("Cat", 2);
+game1.makeGuess("c");
+game1.makeGuess("c");
+
+game1.makeGuess("a");
+game1.makeGuess("z");
+game1.makeGuess("p");
+console.log(game1.getPuzzle());
+const game2 = new Hangman("foot apparel", 200);
+game2.makeGuess('z')
+console.log(game2.numGuesses)
