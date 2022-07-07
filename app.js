e399b5f12af2cf3dfd21a6203e5afc2902d04e14
unknown <sazwerling@gmail.com>
2022-07-07 18:14:12 -0500

    render word and remaining guesses in browser

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const product = {
-    book: 'Jubilation'
-}
+const game1 = new Hangman("Cat", 2);
+const puzzleEl = this.document.querySelector("#word")
+puzzleEl.textContent = game1.getPuzzle()
+const guessesEl = this.document.querySelector("#remaining")
+guessesEl.textContent = game1.numGuesses
 
-console.log(product.hasOwnProperty('hasOwnProperty'))
-console.log('slim'.split('').findIndex(letter => letter === "i"))
+
+
+window.addEventListener('keydown', function(e){
+   if(e.key.match(/^[a-z0-9 ]$/i)){
+      const guess = e.key
+      game1.makeGuess(guess)
+
+      const puzzleEl = this.document.querySelector("#word")
+      puzzleEl.textContent = game1.getPuzzle()
+      const guessesEl = this.document.querySelector("#remaining")
+      guessesEl.textContent = game1.numGuesses
+   }
+  
+})
+
+
+
+
+
--- hangman.js
+++ hangman.js
@@ -1,7 +1,5 @@
-// CREATE A METHOD FOR MAKING A GUESS.
-// Should accept a character for guessing.
-// Should add unique guesses to list of guesses.
-// Should decrement the guesses left if a unique guess is not a match.
+
+
 
 const Hangman = function (word, numGuesses) {
    this.word = word.toLowerCase().split("");
@@ -31,16 +29,3 @@ Hangman.prototype.getPuzzle = function () {
    return puzzle;
 };
 
-const game1 = new Hangman("Cat", 2);
-console.log(game1.getPuzzle())
-console.log(game1.numGuesses)
-
-window.addEventListener('keydown', function(e){
-   if(e.key.match(/^[a-z0-9 ]$/i)){
-      const guess = e.key
-      game1.makeGuess(guess)
-      console.log(game1.getPuzzle())
-      console.log(game1.numGuesses)
-   }
-  
-})
