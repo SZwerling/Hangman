e85ca0cc39fb7423fdbe6ff89b598b8300ce2230
unknown <sazwerling@gmail.com>
2022-07-21 12:31:09 -0500

    http headers and errors

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ request.open('GET', 'http://puzzle.mead.io/puzzle')
 request.send()
 request.addEventListener('readystatechange', (e) => {
   
-   if(e.target.readyState === 4){
+   if(e.target.readyState === 4 && e.target.status === 200){
       const data = JSON.parse(e.target.responseText)
       console.log(data)
+   } else if(e.target.readyState === 4){
+      console.log(`An error has taken place.`)
    }
 })
