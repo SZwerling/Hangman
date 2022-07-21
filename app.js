b6d5e32835737e68ae4ed2e5f97f0599e5f65c36
unknown <sazwerling@gmail.com>
2022-07-21 11:51:46 -0500

    XMLHttpRequest to puzzle.mead

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ window.addEventListener("keydown", function (e) {
       messageEl.textContent = game1.statusMessage; //using getter in Hangman class
    }
 });
+
+// Making an http request
+const request = new XMLHttpRequest()
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
+request.addEventListener('readystatechange', (e) => {
+  
+   if(e.target.readyState === 4){
+      const data = JSON.parse(e.target.responseText)
+      console.log(data)
+   }
+})
